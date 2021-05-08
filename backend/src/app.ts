a9ab828c7a9e52c38e2a6ba3214947492d4afb1a
Emilien GAUTHIER <73469349+EmilienGauthier01@users.noreply.github.com>
2021-05-08 20:03:21 +0200

    add healtcheck

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import exercisesController from "./controllers/exercises.controller";
+import healthcheckController from "./controllers/healthcheck.controller";
 
 export const app = express();
 
@@ -8,5 +9,6 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/exercises", exercisesController);
+app.use("/healthcheck", healthcheckController);
 
 app.listen(4000, () => console.log("Server is now running on port 4000"));
